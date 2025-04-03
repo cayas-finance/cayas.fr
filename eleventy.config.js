766ce47280e458e4f2ce19849b6e59b659316fa4
Thomas Moyse <tmoyse@gmail.com>
2025-04-03 16:00:20 +0200

    image

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
-import svgSprite from "eleventy-plugin-svg-sprite";
+// import svgSprite from "eleventy-plugin-svg-sprite";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import postcss from "postcss";
 import autoprefixer from "autoprefixer";
 import cssnanoPlugin from "cssnano";
@@ -7,6 +8,7 @@ import { charts } from "./scripts/charts.marked.js";
 import { table } from "./scripts/table.marked.js";
 import { legend } from "./scripts/legend.marked.js";
 import { ninja } from "./scripts/ninja.marked.js";
+import { cssImage } from "./scripts/image.postcss.js";
 import katex from "katex";
 
 const R_FootNote = /\[(?<footnote>.{1,3})\]/gm;
@@ -17,10 +19,21 @@ export default async function (cfg) {
   cfg.setInputDirectory("views");
 
   // JavaScript
-  cfg.addPassthroughCopy("js");
+  // cfg.addPassthroughCopy("js");
 
   // Assets
-  cfg.addPassthroughCopy("assets");
+  cfg.addPassthroughCopy("views/assets/favicon");
+  cfg.addPassthroughCopy("views/assets/fonts");
+
+  // Images
+  cfg.addPlugin(eleventyImageTransformPlugin);
+
+  // sprites
+  // cfg.addWatchTarget("views/assets/sprites/*.svg");
+  // cfg.addPlugin(svgSprite, {
+  //   path: "views/assets/sprites", // relative path to SVG directory
+  //   outputFilepath: "_site/assets/sprites/icons.svg",
+  // });
 
   // CSS
   cfg.addTemplateFormats("css");
@@ -28,22 +41,16 @@ export default async function (cfg) {
     outputFileExtension: "css",
     compile: async (inputContent, inputPath) => {
       return async () => {
-        let output = await postcss([autoprefixer, cssnanoPlugin]).process(
-          inputContent,
-          { from: inputPath }
-        );
+        let output = await postcss([
+          autoprefixer,
+          cssnanoPlugin,
+          cssImage,
+        ]).process(inputContent, { from: inputPath });
 
         return output.css;
       };
     },
   });
-
-    // sprites
-    cfg.addWatchTarget("./sprites/*.svg");
-    cfg.addPlugin(svgSprite, {
-        path: "./sprites", // relative path to SVG directory
-        outputFilepath: "./assets/svg-sprite/svg-sprite.svg"
-    });
 
   // Markdown
   cfg.addFilter("markdown", async (content) => {
