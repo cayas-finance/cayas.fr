b15206304f4d89783e15e89523ca6dc7390209b9
Thomas Moyse <tmoyse@gmail.com>
2025-08-19 10:09:38 +0200

    nouvelle version des CGU

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,15 +1,15 @@
-import {eleventyImageTransformPlugin} from "@11ty/eleventy-img";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import postcss from "postcss";
 import autoprefixer from "autoprefixer";
 import cssnanoPlugin from "cssnano";
 import spritePlugin from "./scripts/sprite.eleventy.js";
 import svgPlugin from "./scripts/svg.eleventy.js";
-import {marked} from "marked";
-import {charts} from "./scripts/charts.marked.js";
-import {table} from "./scripts/table.marked.js";
-import {legend} from "./scripts/legend.marked.js";
-import {ninja} from "./scripts/ninja.marked.js";
-import {cssImage} from "./scripts/image.postcss.js";
+import { marked } from "marked";
+import { charts } from "./scripts/charts.marked.js";
+import { table } from "./scripts/table.marked.js";
+import { legend } from "./scripts/legend.marked.js";
+import { ninja } from "./scripts/ninja.marked.js";
+import { cssImage } from "./scripts/image.postcss.js";
 import katex from "katex";
 
 const R_FootNote = /\[(?<footnote>.{1,3})\]/gm;
@@ -17,81 +17,84 @@ const R_Equation = /\$\$(?<equation>[^\$]*)\$\$/gm;
 const R_InlineEquation = /\$(?<equation>[^\$]*)\$/gm;
 
 export default async function (cfg) {
-    cfg.setInputDirectory("views");
+  cfg.setInputDirectory("views");
 
-    // JavaScript
-    cfg.addPassthroughCopy("views/js");
+  // JavaScript
+  cfg.addPassthroughCopy("views/js");
 
-    // Assets
-    cfg.addPassthroughCopy("views/assets/favicon");
-    cfg.addPassthroughCopy("views/assets/fonts");
+  // Assets
+  cfg.addPassthroughCopy("views/assets/favicon");
+  cfg.addPassthroughCopy("views/assets/fonts");
 
-    // Images
-    cfg.addPlugin(eleventyImageTransformPlugin, {
-        formats: ["auto"],
+  // pdfs
+  cfg.addPassthroughCopy("views/assets/files");
+
+  // Images
+  cfg.addPlugin(eleventyImageTransformPlugin, {
+    formats: ["auto"],
+  });
+
+  // sprites
+  cfg.addPlugin(spritePlugin);
+
+  // svg
+  cfg.addPlugin(svgPlugin);
+
+  // CSS
+  cfg.addTemplateFormats("css");
+  cfg.addExtension("css", {
+    outputFileExtension: "css",
+    compile: async (inputContent, inputPath) => {
+      return async () => {
+        let output = await postcss([
+          autoprefixer,
+          cssnanoPlugin,
+          cssImage,
+        ]).process(inputContent, { from: inputPath });
+
+        return output.css;
+      };
+    },
+  });
+
+  // Markdown
+  cfg.addFilter("markdown", async (content) => {
+    // Charts, Tables, Legands, Ninja
+    marked.use(charts(), table(), legend(), ninja());
+
+    // FootNotes
+    content = content.replace(R_FootNote, "");
+
+    // image in localhost
+    content = content.replace("/api", "http://localhost:4200/api");
+
+    // Equations
+    const equations = [];
+    content = content.replaceAll(R_Equation, (_, eq) => {
+      equations.push(eq);
+      return "@EQUATION";
+    });
+    content = content.replaceAll(R_InlineEquation, (_, eq) => {
+      equations.push(eq);
+      return "@EQUATION";
     });
 
-    // sprites
-    cfg.addPlugin(spritePlugin);
+    content = await marked.parse(content);
 
-    // svg
-    cfg.addPlugin(svgPlugin);
-
-    // CSS
-    cfg.addTemplateFormats("css");
-    cfg.addExtension("css", {
-        outputFileExtension: "css",
-        compile: async (inputContent, inputPath) => {
-            return async () => {
-                let output = await postcss([
-                    autoprefixer,
-                    cssnanoPlugin,
-                    cssImage,
-                ]).process(inputContent, {from: inputPath});
-
-                return output.css;
-            };
-        },
+    // Math expression : Convert and replace
+    equations.forEach((equation) => {
+      content = content.replace(
+        "@EQUATION",
+        katex.renderToString(equation, {
+          throwOnError: false,
+          output: "mathml",
+        })
+      );
     });
 
-    // Markdown
-    cfg.addFilter("markdown", async (content) => {
-        // Charts, Tables, Legands, Ninja
-        marked.use(charts(), table(), legend(), ninja());
+    return content;
+  });
 
-        // FootNotes
-        content = content.replace(R_FootNote, "");
-
-        // image in localhost
-        content = content.replace("/api", "http://localhost:4200/api");
-
-        // Equations
-        const equations = [];
-        content = content.replaceAll(R_Equation, (_, eq) => {
-            equations.push(eq);
-            return "@EQUATION";
-        });
-        content = content.replaceAll(R_InlineEquation, (_, eq) => {
-            equations.push(eq);
-            return "@EQUATION";
-        });
-
-        content = await marked.parse(content);
-
-        // Math expression : Convert and replace
-        equations.forEach((equation) => {
-            content = content.replace(
-                "@EQUATION",
-                katex.renderToString(equation, {
-                    throwOnError: false,
-                    output: "mathml",
-                })
-            );
-        });
-
-        return content;
-    });
-
-    // Ignore GitIgnore :)
-    cfg.setUseGitIgnore(false);
+  // Ignore GitIgnore :)
+  cfg.setUseGitIgnore(false);
 }
